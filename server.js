a521e537fd9e7316d35b488b527d608a50ae5c73
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-07-14 15:33:39 -0500

    Add Heroku, and notes route find by id

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+const { notes } = require('./db/db.json');
+
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+
+  if (query.id) {
+    filteredResults = filteredResults.filter(note => note.id === query.id);
+  }
+
+  return filteredResults;
+}
+
+app.get('/api/notes', (req, res) => {
+  let results = notes;
+
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
