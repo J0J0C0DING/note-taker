8356375d9341e5e92eda81a9a56ee85d49519bd4
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-07-14 23:20:55 -0500

    Added routes to find and post notes

--- server.js
+++ server.js
@@ -1,27 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const app = express();
+const shortid = require('shortid');
+
+// Middleware
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
 const { notes } = require('./db/db.json');
 
-function filterByQuery(query, notesArray) {
-  let filteredResults = notesArray;
+function findById(id, notesArray) {
+  const result = notesArray.filter(note => note.id === id)[0];
 
-  if (query.id) {
-    filteredResults = filteredResults.filter(note => note.id === query.id);
-  }
-
-  return filteredResults;
+  return result;
 }
 
-app.get('/api/notes', (req, res) => {
-  let results = notes;
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
 
-  if (req.query) {
-    results = filterByQuery(req.query, results);
+  fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify({ notes: notesArray }, null, 2));
+
+  return note;
+}
+
+// Get all notes
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+// Get not by id
+app.get('/api/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
   }
-  res.json(results);
+});
+
+app.post('/api/notes', (req, res) => {
+  // Set random id using shortid
+  req.body.id = shortid.generate();
+
+  // Add note to json file and notes array
+  const note = createNewNote(req.body, notes);
+
+  res.json(note);
 });
 
 app.listen(PORT, () => {
